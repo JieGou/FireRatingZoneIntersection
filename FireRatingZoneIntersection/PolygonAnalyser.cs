053ef6c8582986491f57b08d2060cb9dacd78ec3
陈杰 <jiegou04@gmail.com>
2024-04-29 05:20:38 +0800

    code format

--- FireRatingZoneIntersection/PolygonAnalyser.cs
+++ FireRatingZoneIntersection/PolygonAnalyser.cs
@@ -3,200 +3,185 @@ using System.Collections.Generic;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using ClipperLib;
-using Polygon = System.Collections.Generic.List<ClipperLib.IntPoint>;
-using Polygons = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
+using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
+using Paths = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
 
 namespace FireRatingZoneIntersection
 {
-  class PolygonAnalyser
-  {
-    //Nearly Entirely the Work of The Builder Coder
-
-    /// <summary>
-    /// Consider a Revit length zero 
-    /// if is smaller than this.
-    /// </summary>
-    const double _eps = 1.0e-9;
-
-    /// <summary>
-    /// Conversion factor from feet to millimetres.
-    /// </summary>
-    const double _feet_to_mm = 25.4 * 12;
-
-    /// <summary>
-    /// Conversion a given length value 
-    /// from feet to millimetres.
-    /// </summary>
-    static long ConvertFeetToMillimetres( double d )
+    internal class PolygonAnalyser
     {
-      if( 0 < d )
-      {
-        return _eps > d
-          ? 0
-          : (long) ( _feet_to_mm * d + 0.5 );
-      }
-      else
-      {
-        return _eps > -d
-          ? 0
-          : (long) ( _feet_to_mm * d - 0.5 );
-      }
-    }
+        //Nearly Entirely the Work of The Builder Coder
 
-    /// <summary>
-    /// Conversion a given length value 
-    /// from millimetres to feet.
-    /// </summary>
-    static double ConvertMillimetresToFeet( long d )
-    {
-      return d / _feet_to_mm;
-    }
+        /// <summary>
+        /// Consider a Revit length zero
+        /// if is smaller than this.
+        /// </summary>
+        private const double _eps = 1.0e-9;
 
-    /// <summary>
-    /// Return a clipper integer point 
-    /// from a Revit model space one.
-    /// Do so by dropping the Z coordinate
-    /// and converting from imperial feet 
-    /// to millimetres.
-    /// </summary>
-    public IntPoint GetIntPoint( XYZ p )
-    {
-      return new IntPoint(
-        ConvertFeetToMillimetres( p.X ),
-        ConvertFeetToMillimetres( p.Y ) );
-    }
+        /// <summary>
+        /// Conversion factor from feet to millimetres.
+        /// </summary>
+        private const double _feet_to_mm = 25.4 * 12;
 
-    /// <summary>
-    /// Return a Revit model space point 
-    /// from a clipper integer one.
-    /// Do so by adding a zero Z coordinate
-    /// and converting from millimetres to
-    /// imperial feet.
-    /// </summary>
-    public XYZ GetXyzPoint( IntPoint p )
-    {
-      return new XYZ(
-        ConvertMillimetresToFeet( p.X ),
-        ConvertMillimetresToFeet( p.Y ),
-        0.0 );
-    }
-
-    /// <summary>
-    /// Retrieve the boundary loops of the given slab 
-    /// top face, which is assumed to be horizontal.
-    /// </summary>
-    Polygons GetBoundaryLoops( CeilingAndFloor slab )
-    {
-      int n;
-      Polygons polys = null;
-      Document doc = slab.Document;
-      Autodesk.Revit.ApplicationServices.Application app = doc.Application;
-
-      Options opt = app.Create.NewGeometryOptions();
-
-      GeometryElement geo = slab.get_Geometry( opt );
-
-      foreach( GeometryObject obj in geo )
-      {
-        Solid solid = obj as Solid;
-        if( null != solid )
+        /// <summary>
+        /// Conversion a given length value
+        /// from feet to millimetres.
+        /// </summary>
+        private static long ConvertFeetToMillimetres(double d)
         {
-          foreach( Face face in solid.Faces )
-          {
-            PlanarFace pf = face as PlanarFace;
-            if( null != pf
-              && pf.FaceNormal.IsAlmostEqualTo( XYZ.BasisZ ) )
+            if (0 < d)
             {
-              EdgeArrayArray loops = pf.EdgeLoops;
+                return _eps > d
+                  ? 0
+                  : (long)(_feet_to_mm * d + 0.5);
+            }
+            else
+            {
+                return _eps > -d
+                  ? 0
+                  : (long)(_feet_to_mm * d - 0.5);
+            }
+        }
 
-              n = loops.Size;
-              polys = new Polygons( n );
+        /// <summary>
+        /// Conversion a given length value
+        /// from millimetres to feet.
+        /// </summary>
+        private static double ConvertMillimetresToFeet(long d)
+        {
+            return d / _feet_to_mm;
+        }
 
-              foreach( EdgeArray loop in loops )
-              {
-                n = loop.Size;
-                Polygon poly = new Polygon( n );
+        /// <summary>
+        /// Return a clipper integer point
+        /// from a Revit model space one.
+        /// Do so by dropping the Z coordinate
+        /// and converting from imperial feet
+        /// to millimetres.
+        /// </summary>
+        public IntPoint GetIntPoint(XYZ p)
+        {
+            return new IntPoint(ConvertFeetToMillimetres(p.X), ConvertFeetToMillimetres(p.Y));
+        }
 
-                foreach( Edge edge in loop )
+        /// <summary>
+        /// Return a Revit model space point
+        /// from a clipper integer one.
+        /// Do so by adding a zero Z coordinate
+        /// and converting from millimetres to
+        /// imperial feet.
+        /// </summary>
+        public XYZ GetXyzPoint(IntPoint p)
+        {
+            return new XYZ(ConvertMillimetresToFeet(p.X), ConvertMillimetresToFeet(p.Y), 0.0);
+        }
+
+        /// <summary>
+        /// Retrieve the boundary loops of the given slab
+        /// top face, which is assumed to be horizontal.
+        /// </summary>
+        private Paths GetBoundaryLoops(CeilingAndFloor slab)
+        {
+            int n;
+            Paths polys = null;
+            Document doc = slab.Document;
+            Autodesk.Revit.ApplicationServices.Application app = doc.Application;
+
+            Options opt = app.Create.NewGeometryOptions();
+
+            GeometryElement geo = slab.get_Geometry(opt);
+
+            foreach (GeometryObject obj in geo)
+            {
+                Solid solid = obj as Solid;
+                if (null == solid)
                 {
-                  IList<XYZ> pts = edge.Tessellate();
-
-                  n = pts.Count;
-
-                  foreach( XYZ p in pts )
-                  {
-                    poly.Add( GetIntPoint( p ) );
-                  }
+                    continue;
                 }
-                polys.Add( poly );
-              }
+                foreach (Face face in solid.Faces)
+                {
+                    PlanarFace pf = face as PlanarFace;
+                    if (null != pf && pf.FaceNormal.IsAlmostEqualTo(XYZ.BasisZ))
+                    {
+                        EdgeArrayArray loops = pf.EdgeLoops;
+
+                        n = loops.Size;
+                        polys = new Paths(n);
+
+                        foreach (EdgeArray loop in loops)
+                        {
+                            n = loop.Size;
+                            Path poly = new Path(n);
+
+                            foreach (Edge edge in loop)
+                            {
+                                IList<XYZ> pts = edge.Tessellate();
+
+                                n = pts.Count;
+
+                                foreach (XYZ p in pts)
+                                {
+                                    poly.Add(GetIntPoint(p));
+                                }
+                            }
+                            polys.Add(poly);
+                        }
+                    }
+                }
             }
-          }
+            return polys;
         }
-      }
-      return polys;
-    }
 
-    public List<CurveArray> Execute(
-      Floor boundary, 
-      Floor eave )
-    {
-      List<CurveArray> Results = new List<CurveArray>();
-
-      Document doc = boundary.Document;
-      Application app = doc.Application;
-
-      // Two slabs to intersect.
-
-      CeilingAndFloor[] slab
-        = new CeilingAndFloor[2] { eave, boundary };
-
-      // Retrieve the two slabs' boundary loops
-
-      Polygons subj = GetBoundaryLoops( slab[0] );
-      Polygons clip = GetBoundaryLoops( slab[1] );
-
-      // Calculate the intersection
-
-      Polygons intersection = new Polygons();
-
-      Clipper c = new Clipper();
-
-      c.AddPaths( subj, PolyType.ptSubject, true );
-      c.AddPaths( clip, PolyType.ptClip, true );
-
-      c.Execute( ClipType.ctIntersection, intersection,
-        PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd );
-
-      // Check for a valid intersection
-
-      if( 0 < intersection.Count )
-      {
-        foreach( Polygon poly in intersection )
+        public List<CurveArray> Execute(Floor boundary, Floor eave)
         {
-          CurveArray curves = app.Create.NewCurveArray();
-          IntPoint? p0 = null; // first
-          IntPoint? p = null; // previous
+            List<CurveArray> Results = new List<CurveArray>();
 
-          foreach( IntPoint q in poly )
-          {
-            if( null == p0 )
+            Document doc = boundary.Document;
+            Application app = doc.Application;
+
+            // Two slabs to intersect.
+            CeilingAndFloor[] slab = new CeilingAndFloor[2] { eave, boundary };
+
+            // Retrieve the two slabs' boundary loops
+            Paths subj = GetBoundaryLoops(slab[0]);
+            Paths clip = GetBoundaryLoops(slab[1]);
+
+            // Calculate the intersection
+            Paths intersection = new Paths();
+
+            Clipper c = new Clipper();
+
+            c.AddPaths(subj, PolyType.ptSubject, true);
+            c.AddPaths(clip, PolyType.ptClip, true);
+
+            c.Execute(ClipType.ctIntersection, intersection,
+              PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd);
+
+            // Check for a valid intersection
+            if (0 < intersection.Count)
             {
-              p0 = q;
+                foreach (Path poly in intersection)
+                {
+                    CurveArray curves = app.Create.NewCurveArray();
+                    IntPoint? p0 = null; // first
+                    IntPoint? p = null; // previous
+
+                    foreach (IntPoint q in poly)
+                    {
+                        if (null == p0)
+                        {
+                            p0 = q;
+                        }
+                        if (null != p)
+                        {
+                            curves.Append(Line.CreateBound(GetXyzPoint(p.Value), GetXyzPoint(q)));
+                        }
+                        p = q;
+                    }
+                    Results.Add(curves);
+                }
             }
-            if( null != p )
-            {
-              curves.Append(
-                Line.CreateBound(
-                  GetXyzPoint( p.Value ),
-                  GetXyzPoint( q ) ) );
-            }
-            p = q;
-          }
-          Results.Add( curves );
+            return Results;
         }
-      }
-      return Results;
     }
-  }
 }
